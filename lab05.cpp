3c68f54af82bb129014da58253fa2aaa52dab34d
M2MAlove <93481311+M2MAlove@users.noreply.github.com>
2022-12-07 02:55:15 +0300

    Firtst submit.

---
+++ lab05.cpp
@@ -0,0 +1,21 @@
+#include "taskold/oldtask.h"
+#include "task1/calculatortask.h"
+#include "task2/sortstask.h"
+#include "task3/stringstask.h"
+#include "structures/booktask.h"
+
+using namespace std;
+
+int main() {
+	//OldTask();
+
+	//First();
+
+	//SecondA();
+	//SecondB();
+	//SecondC();
+
+	//ThirdTask();
+
+	//BookTask();
+}
